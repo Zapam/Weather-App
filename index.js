acbdf6c5b31f3a87e0d96c7d26d7142060a6b135
AmirZapam <amirzapam9@gmail.com>
2023-11-16 13:10:25 +0330

    Weather App

--- index.js
+++ index.js
@@ -6,18 +6,41 @@ let cityName = document.querySelector(".city");
 let temp = document.querySelector(".temp");
 let humidity = document.querySelector(".humidity");
 let wind = document.querySelector(".wind");
+let weatherIcon = document.querySelector(".weather-icon") 
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 
 async function checkWeather(city) {
 	const response = await fetch(apiURL + city + `&appid=${apiKey}`);
-	let data = await response.json();
-	console.log(data);
-	cityName.innerHTML = data.name;
-	temp.innerHTML = Math.round(data.main.temp) + "℃";
-	humidity.innerHTML = data.main.humidity + "%";
-	wind.innerHTML = data.wind.speed + "km/h";
+	if (response.status == 404)  {
+		document.querySelector(".error").style.display = "block";
+		document.querySelector(".weather").style.display = "none";
+	} else {
+		let data = await response.json();
+		cityName.innerHTML = data.name;
+		temp.innerHTML = Math.round(data.main.temp) + "℃";
+		humidity.innerHTML = data.main.humidity + "%";
+		wind.innerHTML = data.wind.speed + "km/h";
+	
+		if (data.weather[0].main === 'Clouds') {
+			weatherIcon.src = "images/clouds.png"
+		} else if (data.weather[0].main === "Clear") {
+			weatherIcon.src = "images/clear.png"
+		}else if (data.weather[0].main === "Rain") {
+			weatherIcon.src = "images/rain.png"
+		}else if (data.weather[0].main === "Drizzle") {
+			weatherIcon.src = "images/drizzle.png"
+		} else if (data.weather[0].main === "Mist") {
+			weatherIcon.src = "images/mist.png"
+		}
+	
+		document.querySelector(".weather").style.display = "block";
+		document.querySelector(".error").style.display = "none";
+
+	}
+	
+
 }
 
 searchBtn.addEventListener("click", () => {
